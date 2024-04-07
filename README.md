f994a708f4c1e4248aef91446f967d76f9668f44
oxygen-TW <r12945029@ntu.edu.tw>
2024-04-07 16:29:14 +0800

    [Update] Update README.md and add data_tool.py

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # DLMI_course_project
-Course project of Deep Learning in Medical Image
+Course project of Deep Learning in Medical Image.
 
 # Project structure
 
@@ -19,3 +19,8 @@ Course project of Deep Learning in Medical Image
     - Test
         - Normal
         - Covid
+
+## Dataset Citation
+L. Wang, Z. Lin, A. Wong. "COVID-Net: a tailored deep convolutional neural network design for detection of COVID-19 cases from chest X-ray images," in Scientific Reports, vol. 10, no. 1, pp. 19549, 2020.
+
+https://github.com/lindawangg/COVID-Net
---
+++ data_tool.py
@@ -0,0 +1,96 @@
+import os
+import random
+
+def load_data(data_root="", use="train", data_type="normal"):
+    """
+    Load data from data_root and return a list of all the files in the specified data path.
+    """
+
+    use = use.lower()
+    data_type = data_type.lower()
+
+    assert use in ["train", "val", "test"], "use parameter must be \"train\", \"val\", or \"test\""
+    assert data_type  in ["normal", "covid"], "use parameter must be \"normal\", \"covid\""
+
+    #check if data_root exists
+    if not os.path.exists(data_root):
+        raise FileNotFoundError("Data root does not exist")
+    
+    data_path = os.path.join(data_root, use, data_type)
+
+    if not os.path.exists(data_path):
+        raise FileNotFoundError("Data path does not exist")
+    
+    #Return the all the files in the data path
+    return os.listdir(data_path)
+    
+def randomly_load_data(data_root="", split_ratio=(0.7, 0.15, 0.15), random_seed=None):
+    """
+    Randomly load data from data_root and split it into train, val, and test sets.
+    The data is split based on the split_ratio parameter.
+    The data is shuffled before splitting.
+    The random seed can be set for reproducibility.
+    """
+
+    assert len(split_ratio) == 3, "split_ratio must be a tuple of 3 elements"
+    assert sum(split_ratio) == 1, "split_ratio must sum to 1"
+
+    random.seed(random_seed)
+
+    use = ["train", "val", "test"]
+    data_type = ["normal", "covid"]
+
+    normal_data = []
+    covid_data = []
+
+    for u in use:
+        for d in data_type:
+            data = load_data(data_root, u, d)
+            if d == "normal":
+                normal_data.extend(data)
+            else:
+                covid_data.extend(data)
+
+    random.shuffle(normal_data)
+    random.shuffle(covid_data)
+    
+    splitted_data = {
+            "train": {
+                "normal": normal_data[:int(split_ratio[0] * len(normal_data))],
+                "covid": covid_data[:int(split_ratio[0] * len(covid_data))]
+            },
+            "val": {
+                "normal": normal_data[int(split_ratio[0] * len(normal_data)):int((split_ratio[0] + split_ratio[1]) * len(normal_data))],
+                "covid": covid_data[int(split_ratio[0] * len(covid_data)):int((split_ratio[0] + split_ratio[1]) * len(covid_data))]
+            },
+            "test": {
+                "normal": normal_data[int((split_ratio[0] + split_ratio[1]) * len(normal_data)):],
+                "covid": covid_data[int((split_ratio[0] + split_ratio[1]) * len(covid_data)):]
+            }
+        }
+    
+    return {
+        "info":{
+            "train": {
+                "normal": len(splitted_data["train"]["normal"]),
+                "covid": len(splitted_data["train"]["covid"])
+            },
+            "val": {
+                "normal": len(splitted_data["val"]["normal"]),
+                "covid": len(splitted_data["val"]["covid"])
+            },
+            "test": {
+                "normal": len(splitted_data["test"]["normal"]),
+                "covid": len(splitted_data["test"]["covid"])
+            },
+            "total": len(normal_data) + len(covid_data)
+        },
+        "data": splitted_data
+        }
+
+
+
+if(__name__ == "__main__"):
+    DATA_ROOT = "/Group16T/raw_data/covid_cxr/"
+    # print(load_data(data_root=DATA_ROOT, use="test", data_type="COVID"))
+    print(randomly_load_data(data_root=DATA_ROOT, split_ratio=(0.7, 0.15, 0.15)))
